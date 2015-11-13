7c9e369a4122c21f3a4243b7bdaab57af02ca1c1
hscsf <helge.schneider@udo.edu>
2015-11-13 18:33:36 +0100

    Login Rahmen / Server Access / Search
    Zur Ausführung wird "json-server" auf "localhost" benötigt. Einfach mit
    "json-server js\db.json --static ." im Verzeichnis der "index.html"
    starten.

--- prototype/web/js/tdapp.js
+++ prototype/web/js/tdapp.js
@@ -52,54 +52,56 @@ tdapp.factory("Fact",function(){
 	}
 	
 	function _setTodos(todolist){
-		todos=todolist;
+		todos = todolist;
+	}
+
+	function _clearTodos(){
+		while(todos.length>0){
+			todos.pop();
+		}
+	}
+	
+	function _getTodoById(id){
+		var ret = undefined;
+		todos.forEach(function(obj){
+			if(obj.id==id){
+				ret=obj;
+			}
+		});
+		return ret;
 	}
 	
 	function _addTodoObj(obj){
 		todos.unshift(obj);
 	}
 	
-	function _addTodo(todotxt){
-		var tmp_ts = Date.now();
-		var tmp_tshr = fkts_tsHr(tmp_ts); 
-		var obj = {
-			ts:tmp_ts,
-			tshr:tmp_tshr,
-			txt:todotxt,
-			done:0
-		};
-		todos.unshift(obj);
-	}
-
 	function _delTodo(item){
 		var idx = todos.indexOf(item)
 		todos.splice(idx,1);		
 	}
 
-	function _togDoneTodo(item){
+	function _togCompleted(item){
 		var idx = todos.indexOf(item);
 		var todo = todos[idx];
-		if(todo.done==0){
-			todo.done = 1;
+		if(todo.completed){
+			todo.completed = false;
 		} else {
-			todo.done = 0;
+			todo.completed = true;
 		}
 	}
-
-	_addTodo("Immer schön speichern!");
-	todos[0].done = 1;
 
 	// Returns
 	
 	return{
 		log : _log,
 		getLog : _getLog,
-		addTodo : _addTodo,
-		addTodoObj : _addTodoObj,
 		getTodos : _getTodos,
-		delTodo : _delTodo,
-		togDoneTodo : _togDoneTodo,
 		setTodos : _setTodos,
+		clearTodos : _clearTodos,
+		getTodoById : _getTodoById,
+		addTodoObj : _addTodoObj,
+		delTodo : _delTodo,
+		togCompleted : _togCompleted,
 	};
 		
 });
@@ -107,7 +109,7 @@ tdapp.factory("Fact",function(){
 /*
   Main controller ----------------------------------------
 */
-tdapp.controller("MainCtrl",function($scope,$timeout,$http,Fact){
+tdapp.controller("MainCtrl",function($scope,$timeout,$interval,$http,Fact){
 
 	// init
 
@@ -115,11 +117,107 @@ tdapp.controller("MainCtrl",function($scope,$timeout,$http,Fact){
 	$scope.log = Fact.getLog();
 
 	$scope.s_login = 1;
+	$scope.s_working = 0;
 	$scope.s_list = 0;
 	$scope.s_add = 0;
 
-	// keyboard functions
+	// communication with server
 	
+	var server = "http://localhost:3000/tasks";
+
+	function errorCallback(response) {
+		Fact.log("Error!");
+		Fact.log("Check browser console for details.");				
+		console.log(JSON.stringify(response));
+	}
+
+	function gettodos(){
+		Fact.log("Sending request (get) to server...");
+		$scope.s_working=1;
+		$scope.s_list=0;
+		$http({
+			method:"get",
+			url: server
+		}).then(
+			function successCallback(response) {
+				Fact.log("Done.");
+				Fact.log("Updating local Todo-List.");
+				response.data.forEach(function(o){
+					Fact.addTodoObj({
+						"id":o.id,
+						"subject":o.subject,
+						"completed":o.completed
+					});
+				});
+				Fact.log("Done.");
+				$timeout(function(){
+					$scope.s_list=1;
+					$scope.s_working=0;
+				},1000);
+			}
+			,
+			function(response) {
+				Fact.log("Error!");
+				Fact.log("Check browser console for details.");				
+				console.log(JSON.stringify(response));
+				$scope.errormsg="Server not available!";
+				$scope.dologout();
+			}
+		);
+	}
+	$scope.gettodos=gettodos;
+
+	function posttodo(obj){
+		Fact.log("Sending request (post) to server...");
+		$http({
+			method:"post",
+			url: server,
+			header: "application/json",
+			data: obj
+		}).then(
+			function successCallback(response) {
+				Fact.log("Done.");
+				obj.id=response.data.id;
+			}
+			,
+			errorCallback
+		);
+	}	
+
+	function puttodo(obj){
+		Fact.log("Sending request (put) to server...");
+		$http({
+			method:"put",
+			url: server+"/"+obj.id,
+			header: "application/json",
+			data: obj
+		}).then(
+			function successCallback(response) {
+				Fact.log("Done.");
+			}
+			,
+			errorCallback
+		);
+	}	
+
+	function deletetodo(obj){
+		Fact.log("Sending request (delete) to server...");
+		$http({
+			method:"delete",
+			url: server+"/"+obj.id,
+			header: "application/json",
+			data: obj
+		}).then(
+			function successCallback(response) {
+				Fact.log("Done.");
+			}
+			,
+			errorCallback
+		);
+	}	
+		
+	// Keyboard functions
+
 	$scope.mainKeydown=function(e){
 		if($scope.s_login==0){
 			var k = e.keyCode;
@@ -134,16 +232,20 @@ tdapp.controller("MainCtrl",function($scope,$timeout,$http,Fact){
 		}
 	}	
 
-	$scope.newtodoKeydown=function(e,newtodotxt){
+	$scope.newtodoKeydown=function(e){
 		var k = e.keyCode;
 		if(k==13){//ret
 			e.preventDefault();
-			Fact.addTodo(newtodotxt);
+			var newtodo = {
+				"subject":$scope.newtodotxt,
+				"completed":false
+			};
 			$scope.newtodotxt = "";
 			$scope.s_add = 0;
 			$scope.s_list = 1;
+			posttodo(newtodo);
+			Fact.addTodoObj(newtodo);
 			window.scrollTo(0,0);
-			Fact.log("Todo added.");
 		} else 
 		if(k==27){//esc
 			e.preventDefault();			
@@ -161,28 +263,53 @@ tdapp.controller("MainCtrl",function($scope,$timeout,$http,Fact){
 		var k=e.keyCode;
 		if(k==13 || k==27 || k==107){//ret,esc,+
 			e.preventDefault();
-			document.getElementById("todoid"+id).blur();
+			Fact.log("Change Todo (id:"+id+").");
+			var currentTodo = document.getElementById("todoid"+id); 
+			currentTodo.blur();
 			Fact.log("Todo unfocused.");
+			
+			var obj = Fact.getTodoById(id);
+			if(obj!=undefined){
+				Fact.log("Done.");
+				Fact.log("Updating data on server.");
+				obj.subject=currentTodo.innerHTML;
+				puttodo(obj);
+				Fact.log("Todo (id:"+id+") updated.");			
+			} else {
+				Fact.log("Error.");
+			}
 		}
 	}
 
-	// todo functions
+	// Todo-Functions
 	
 	$scope.seltodoline=function(id){
 		document.getElementById("todoid"+id).focus();
 		Fact.log("Todo focused.");
 	}	
 	
-	$scope.deltodo=function(item){
-		Fact.delTodo(item);
-		Fact.log("ToDo deleted.");
+	$scope.deltodo=function(obj){
+		obj.opac=1;
+		obj.del=false;
+		$interval(function(){
+			var x = document.getElementById("todox"+obj.id);
+			obj.opac = obj.opac - 0.05;
+			x.style.opacity = obj.opac;
+			if(obj.opac<=0 && !obj.deleted){
+				obj.deleted=true;
+				deletetodo(obj);
+				//Fact.delTodo(obj);
+				Fact.log("ToDo deleted.");			
+			}
+		},32,32);
 		if(Fact.getTodos().length==0){
 			Fact.log("Todo-List is empty.");			
 		}
 	}
 
-	$scope.togDone=function(item){
-		Fact.togDoneTodo(item);
+	$scope.togCompleted=function(obj){
+		Fact.togCompleted(obj);
+		puttodo(obj);
 		Fact.log("Todo-Flag changed.");
 	}
 
@@ -195,50 +322,41 @@ tdapp.controller("MainCtrl",function($scope,$timeout,$http,Fact){
 		Fact.log("New Todo-Dialog.");
 	}
 	
-	$scope.gettodos=function(){
-		Fact.log("Getting data from server...");
-		$http({
-			method:"get",
-			url: "http://www.drtodolittle.de:8181/rest-api/tasks"
-		}).then(
-			function successCallback(response) {
-				Fact.log("Done.");
-				Fact.log("Updateing Todo-List.");
-				response.data.forEach(function(o){
-					var nobj = {
-						ts:0,
-						tshr:o.id,
-						txt:o.subject,
-						done:o.completed					
-					}
-					Fact.addTodoObj(nobj);
-				});
-				Fact.log("Done.");
-			}
-			,
-			function errorCallback(response) {
-				Fact.log("Error!");
-				Fact.log("Check browser console for details.");				
-				console.log(JSON.stringify(response));
-			}
-		);
-	}
-		
-	// login functions
-		
-	$scope.dologin=function(){
+	// Login functions
+			
+	function logout(){
 		$scope.s_login=0;
-		$scope.s_list=1;
-		Fact.log("Logged in.");
-	}
-	
-	$scope.dologout=function(){
-		$scope.s_login=1;
 		$scope.s_list=0;
 		$scope.s_add=0;
+		$scope.s_working=1;
+		Fact.clearTodos();
+		$timeout(function(){
+			$scope.s_working=0;
+			$scope.s_login=1;
+		},1000);
 		Fact.log("Logged out.");
 	}
+	$scope.dologout=logout;
 
+	function login(){
+		if($scope.liusername=="gast"){
+			Fact.log("Logged in.");		
+			$scope.errormsg="";
+			$scope.s_login=0;
+			$scope.gettodos();
+		} else {
+			$scope.errormsg="Username/Password error!";
+			$scope.dologout();
+		}
+	}
+	$scope.dologin=login;
+	
+	// Init
+	
 	Fact.log("Client Log-System ready.");
+	
+	if($scope.s_list==1){
+		$scope.gettodos();
+	}
 	
 });
